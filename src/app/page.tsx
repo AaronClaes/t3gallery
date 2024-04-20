a247bb11194f81ccf2d00a1f14576a6f72a1c9ef
= <aaronclaes22@gmail.com>
2024-04-20 15:22:54 +0200

    feat: break queries into separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { db } from "~/server/db";
-import { TopNav } from "./_components/topnav";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
   return (
     <div className="flex flex-wrap gap-4">
       {images.map((image) => (
@@ -23,7 +20,14 @@ async function Images() {
 export default function HomePage() {
   return (
     <main className="">
-      <Images />
+      <SignedOut>
+        <div className="center h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
