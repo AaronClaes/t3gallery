4234ee724dd60f188ec105eaa0423319fd6b3f92
= <aaronclaes22@gmail.com>
2024-04-20 22:02:59 +0200

    feat: upload event capture in posthog

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
 import { toast } from "sonner";
+import { usePostHog } from "posthog-js/react";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -53,8 +54,11 @@ function UploadSVG() {
 
 export function SimpleUploadButton() {
   const router = useRouter();
+  const posthog = usePostHog();
+
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
+      posthog.capture("upload_begin");
       toast.loading("Uploading...", {
         duration: 100000,
         id: "upload-begin",
